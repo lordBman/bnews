ad875f7b5a138600d92b62ddbf7cb3287847b233
lordBman <okelekelenobel@gmail.com>
2025-01-30 08:54:56 +0100

    added screen and page placeholder

--- app/src/main/java/com/bsoft/bnews/MainActivity.kt
+++ app/src/main/java/com/bsoft/bnews/MainActivity.kt
@@ -8,7 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.bsoft.bnews.ui.screens.InfoScreen
+import com.bsoft.bnews.ui.screens.LoadingScreen
+import com.bsoft.bnews.ui.screens.MainScreen
 import com.bsoft.bnews.ui.theme.BNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,4 +35,38 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+enum class Screens(val route: String) {
+    Loading(route = "Loading"),
+    Main(route = "Main"),
+    Info(route = "Info")
+}
+
+@Composable
+fun App(){
+    BNewsTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = Screens.Loading.route , builder = {
+                composable(route = Screens.Loading.route) {
+                    LoadingScreen()
+                }
+                composable(route = Screens.Main.route) {
+                    MainScreen()
+                }
+                composable(route = Screens.Info.route) {
+                    InfoScreen()
+                }
+            } )
+
+        }
+    }
+}
+
+@Preview(name = "App Light Mode")
+@Composable
+fun AppPreview(){
+    App()
 }
---
+++ app/src/main/java/com/bsoft/bnews/ui/pages/settings.kt
@@ -0,0 +1,20 @@
+package com.bsoft.bnews.ui.pages
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingsPage(){
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+@Composable
+@Preview
+fun SettingsPagePreview(){
+    SettingsPage()
+}
